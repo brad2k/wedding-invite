a7e11669eb35d3e17497e70b397d267c3fabd8cc
Brad Azevedo <bazevedo@jawbone.com>
2015-04-14 22:58:36 -0700

    More setup work

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var register = require('./register');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Test' });
+});
+
+/* Route for registering an email address */
+router.post('/register', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
