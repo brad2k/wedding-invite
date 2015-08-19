9ad6e11abe5ca03b5df20546d231bc9dc3454f06
Brad Azevedo <bazevedo@jawbone.com>
2015-08-19 00:22:12 -0700

    Optimize Flickr as much as I can

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,9 @@
-require(["jquery", "vendor/picturefill", "vendor/fontfaceobserver", "smoothscroll"], function($, picturefill, fontfaceobserver, smoothscroll) {
+require(["require", "jquery", "vendor/fontfaceobserver", "smoothscroll"], function(require, $, fontfaceobserver, smoothscroll) {
     
     // Email registration form
-
     var emailForm = $('#email-submission');
-
     emailForm.on('submit', function (e) {
         e.preventDefault();
-
         $.ajax({
             url: this.action,
             method: "GET",
@@ -15,22 +12,40 @@ require(["jquery", "vendor/picturefill", "vendor/fontfaceobserver", "smoothscrol
             dataType: 'json',
         }).done(function(response) {
             emailForm.addClass('success');
-        }).error(function (response) {
-            console.log(response);
         });
     });
 
-    if( window.document.documentElement.className.indexOf( "fonts-loaded" ) > -1 ){
-        return;
-    }
-    var fontA = new window.FontFaceObserver( "Neutratext Book Italic");
-    var fontB = new window.FontFaceObserver( "Neutratext Bold");
-    var fontC = new window.FontFaceObserver( "Neutra Display Titling");
-    var fontD = new window.FontFaceObserver( "Neutraface Condensed Titling");
+
+    // Conditional font loading
+    var fontA = new window.FontFaceObserver( "Neutratext Book Italic"),
+        fontB = new window.FontFaceObserver( "Neutratext Bold"),
+        fontC = new window.FontFaceObserver( "Neutra Display Titling"),
+        fontD = new window.FontFaceObserver( "Neutraface Condensed Titling");
 
     window.Promise
     .all([fontA.check(), fontB.check(), fontC.check(), fontD.check()])
     .then(function(){
         window.document.documentElement.className += " fonts-loaded";
     });
+
+
+    // Flickr
+    var mq = window.matchMedia,
+        $win = $(window),
+        checkSize = function (e) {
+            var data = e ? e.data : false;
+            if (mq("(min-width: 768px)").matches) {
+                require(['//embedr.flickr.com/assets/client-code.js']);
+                if (data && data.disableWindowEvent) {
+                    $win.off('resize', checkSize);
+                }
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+    if (mq) {
+        checkSize() || $win.on('resize', {'disableWindowEvent': true}, checkSize);
+    }
 });
